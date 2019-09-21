49bc180adc17b94c94880060b5016fb7a674e53c
risottoupdate <freepc.yuki@gmail.com>
2019-09-21 15:41:08 +0900

    human_csv_import

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ApplicationRecord
 	# CSVデータのパスを引数として受け取り、インポート処理を実行
-  def self.import(path)
+  def self.import
+
+  	# CSVを格納しているディレクトリをpathに保存
+  	path = File.join Rails.root, "db/csv_data/human.csv"
+
+    puts "path: #{path}"
   	list = []
   	CSV.foreach(path, headers: true) do |row|
 	  	list << {
-	  		name: row["name"],
-	  		age: row["age"],
-	  		address: row["address"]
+	  		Address_id: row["Address_id"],
+	  		Address: row["Address"],
+	  		Age: row["Age"],
+	  		Year: row["Year"],
+	  		Human: row["Human"],
+	  		Human_man: row["Human_man"],
+	  		Human_woman: row["Human_woman"]
 	  	}
 	  end
 	  User.create(list)
---
+++ db/migrate/20190921063449_add_column_user.rb
@@ -0,0 +1,21 @@
+class AddColumnUser < ActiveRecord::Migration[6.0]
+  def change
+  	remove_column :users, :都道府県コード, :string
+  	remove_column :users, :都道府県名, :string
+  	remove_column :users, :年齢5歳階級, :string
+  	remove_column :users, :元号, :string
+  	remove_column :users, :和暦, :integer
+  	remove_column :users, :西暦, :integer
+  	remove_column :users, :人口, :integer
+  	remove_column :users, :人口_男, :integer
+  	remove_column :users, :人口_女, :integer
+
+  	add_column :users, :Address_id, :integer
+  	add_column :users, :Address, :string
+  	add_column :users, :Age, :integer
+  	add_column :users, :Year, :integer
+  	add_column :users, :Human, :integer
+  	add_column :users, :Human_man, :integer
+  	add_column :users, :Human_woman, :integer
+  end
+end
