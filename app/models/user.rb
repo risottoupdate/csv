5eec65e869c9ebef6821245bce609306b706a414
risottoupdate <freepc.yuki@gmail.com>
2019-09-21 14:41:27 +0900

    edit_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 	# CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
+  	list = []
   	CSV.foreach(path, headers: true) do |row|
-	  	User.create(
+	  	list << {
 	  		name: row["name"],
 	  		age: row["age"],
 	  		address: row["address"]
-	  		)
+	  	}
 	  end
+	  User.create(list)
   end
 end
